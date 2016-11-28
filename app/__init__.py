98ac01695cc6848bb54650510051803f4c6e4782
SandeepPandey <s4n989@gmail.com>
2016-11-28 21:20:20 +0530

    Added support for create and upload csv files to s3

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,20 @@
 ##
 
 import rds_handler
-
+import time
+from datetime import datetime, date
+import dateutil.parser
 
 def create_rds_snapshot_instance():
     print("Creating rds instance using snapshot")
     rds_handler_object = rds_handler.RdsHandler()
     latest_snapshot = rds_handler_object.get_snapshot_list()
-    # rds_handler_object.create_rds_instance_from_snapshot(latest_snapshot)
-    rds_handler_object.create_redshift_schema()
+    print latest_snapshot
+    print latest_snapshot["SnapshotCreateTime"]
+    print time.mktime(latest_snapshot["SnapshotCreateTime"].timetuple())
+    print datetime.utcfromtimestamp(time.mktime(latest_snapshot["SnapshotCreateTime"].timetuple()))
+    #time.mktime(latest_snapshot["SnapshotCrateTime"])
+    #rds_handler_object.create_rds_instance_from_snapshot(latest_snapshot)
+    #rds_handler_object.create_redshift_schema()
+    #rds_handler_object.generate_data_csv_file()
+    rds_handler_object.upload_csv_files_to_s3()
--- app/rds_handler.py
+++ app/rds_handler.py
@@ -4,11 +4,12 @@
 
 import boto3
 import config
-import time
 import MySQLdb
 import MySQLdb.cursors
 import _mysql_exceptions
 import sys
+import math
+import csv
 
 tython_config = config.Config()
 conf = tython_config.get_config()
@@ -18,10 +19,14 @@ conn = boto3.client('rds',
                         'aws', 'aws_secret_access_key'),
                     region_name=conf.get('aws', 'region_name'))
 suffix = '1479893180'
-
+tables = ["applicant_profile", "applicant_profile_version", "applicant_status", "applicants",
+                  "applicants_version", "candidate_metadata", "companies", "cv_status", "degrees", "feature_request",
+                  "interview_details", "interview_details_version", "interview_status", "interview_type",
+                  "interviewer_interview_relations", "interviewer_status", "load_data", "project_requisition_relations",
+                  "projects", "requisition", "roles", "rounds", "sources", "test", "transaction", "universities",
+                  "user_requisition_relations", "users"]
 
 class RdsHandler:
-
     def get_snapshot_list(object):
         snapshots = conn.describe_db_snapshots(
             DBInstanceIdentifier=conf.get('aws', 'databases'),
@@ -33,7 +38,7 @@ class RdsHandler:
 
     def create_rds_instance_from_snapshot(object, latest_snapshot):
         db_instance_identifier = 'tython-' + conf.get('aws', 'databases') + \
-            "-" + suffix
+                                 "-" + suffix
         response = conn.restore_db_instance_from_db_snapshot(
             DBSnapshotIdentifier=latest_snapshot['DBSnapshotIdentifier'],
             DBInstanceIdentifier=db_instance_identifier,
@@ -41,65 +46,41 @@ class RdsHandler:
             MultiAZ=False
         )
         print response
-        #{u'DBInstance': {u'PubliclyAccessible': True, u'MasterUsername': 'hireninjabuilder', u'MonitoringInterval': 0, u'LicenseModel': 'general-public-license', u'VpcSecurityGroups': [{u'Status': 'active', u'VpcSecurityGroupId': 'sg-62110200'}], u'CopyTagsToSnapshot': False, u'OptionGroupMemberships': [{u'Status': 'pending-apply', u'OptionGroupName': 'default:mysql-5-6'}], u'PendingModifiedValues': {}, u'Engine': 'mysql', u'MultiAZ': False, u'DBSecurityGroups': [], u'DBParameterGroups': [{u'DBParameterGroupName': 'default.mysql5.6', u'ParameterApplyStatus': 'in-sync'}], u'AutoMinorVersionUpgrade': True, u'PreferredBackupWindow': '18:48-19:18', u'DBSubnetGroup': {u'Subnets': [{u'SubnetStatus': 'Active', u'SubnetIdentifier': 'subnet-496d7d2b', u'SubnetAvailabilityZone': {u'Name': 'ap-southeast-1b'}}, {u'SubnetStatus': 'Active', u'SubnetIdentifier': 'subnet-f4dcec80', u'SubnetAvailabilityZone': {u'Name': 'ap-southeast-1a'}}], u'DBSubnetGroupName': 'default', u'VpcId': 'vpc-11d4c373', u'DBSubnetGroupDescription': 'default', u'SubnetGroupStatus': 'Complete'}, u'ReadReplicaDBInstanceIdentifiers': [], u'AllocatedStorage': 5, u'DBInstanceArn': 'arn:aws:rds:ap-southeast-1:754922593538:db:tython-hireninja-1479893180', u'BackupRetentionPeriod': 1, u'DBName': 'hireninja', u'PreferredMaintenanceWindow': 'sun:14:54-sun:15:24', u'DBInstanceStatus': 'creating', u'EngineVersion': '5.6.23', u'DomainMemberships': [], u'StorageType': 'gp2', u'DbiResourceId': 'db-P6D2EP3I6NCQN6ASYKUNQLSCKI', u'CACertificateIdentifier': 'rds-ca-2015', u'StorageEncrypted': False, u'DBInstanceClass': 'db.m1.small', u'DbInstancePort': 0, u'DBInstanceIdentifier': 'tython-hireninja-1479893180'}, 'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': '68417af1-b15f-11e6-a094-49e1e0014924', 'HTTPHeaders': {'x-amzn-requestid': '68417af1-b15f-11e6-a094-49e1e0014924', 'vary': 'Accept-Encoding', 'content-length': '3376', 'content-type': 'text/xml', 'date': 'Wed, 23 Nov 2016 09:30:03 GMT'}}}
 
-    def create_redshift_schema(object):
-    	col_map = {
-                "INTEGER": "INTEGER",
-                "INT": "INTEGER",
-                "TINYINT": "SMALLINT",
-                "SMALLINT": "SMALLINT",
-                "MEDIUMINT": "INTEGER",
-                "BIGINT": "BIGINT",
-                "DECIMAL": "DECIMAL",
-                "NUMERIC": "DECIMAL",
-                "FLOAT": "FLOAT",
-                "DOUBLE": "FLOAT",
-                "DATE": "DATE",
-                "TIME": "VARCHAR",
-                "YEAR": "SMALLINT",
-                "DATETIME": "TIMESTAMP",
-                "TIMESTAMP": "TIMESTAMP",
-                "CHAR": "VARCHAR",
-                "VARCHAR": "VARCHAR",
-                "BINARY": "VARCHAR",
-                "VARBINARY": "VARCHAR",
-                "BLOB": "VARCHAR",
-                "TINYBLOB": "VARCHAR",
-                "MEDIUMBLOB": "VARCHAR",
-                "LONGBLOB": "VARCHAR",
-                "TEXT": "VARCHAR",
-                "TINYTEXT": "VARCHAR",
-                "MEDIUMTEXT": "VARCHAR",
-                "LONGTEXT": "VARCHAR",
-                "ENUM": "VARCHAR",
-                "SET": "VARCHAR",
-                "BIT": "VARCHAR",
-                "POLYGON": "UNSUPPORTED"
-                }
-        print "Generating Schema DDS and Selector SQL Statements"
+    def generate_data_csv_file(self):
+        cursor = None
         try:
             rds = MySQLdb.connect(
-            	user=conf.get('hireninja', 'user'),
-            	passwd=conf.get('hireninja', 'password'),
-            	host=conf.get('hireninja', 'host'),
-            	port=int(conf.get('hireninja', 'port')),
-            	db=conf.get('hireninja', 'db'),
-            	cursorclass=MySQLdb.cursors.DictCursor)
+                user=conf.get('hireninja', 'user'),
+                passwd=conf.get('hireninja', 'password'),
+                host=conf.get('hireninja', 'host'),
+                port=int(conf.get('hireninja', 'port')),
+                db=conf.get('hireninja', 'db'),
+                cursorclass=MySQLdb.cursors.DictCursor)
             rds_cursor = rds.cursor()
-            rds_cursor.execute("SELECT c.*, t.TABLE_ROWS, " +
-                                       "ceil(((t.data_length+t.index_length)/(1024*1024))) as T_SIZE, " +
-                                       "if(t.TABLE_ROWS > 0,ceil(ceil(((t.data_length+t.index_length)))/t.TABLE_ROWS),0) as R_SIZE FROM " +
-                                       "information_schema.columns c LEFT JOIN information_schema.tables t " +
-                                       "ON t.table_name = c.table_name and t.table_schema = c.table_schema " +
-                                       "WHERE t.TABLE_TYPE='BASE TABLE' AND c.table_schema = '" + conf.get('hireninja', 'db') +
-                                       "' order by c.table_name, c.ordinal_position")
+            for table in tables:
+                rds_cursor.execute("select * from " + table);
+                result = rds_cursor.fetchall()
+                myfile = open("/Users/sandeep/Documents/project/code/experiments/tython/csv_files/" + table + ".csv", 'wb')
+                wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+                wr.writerow(result)
         except _mysql_exceptions.Error, err:
-        	print (err)
-        	sys.exit(1)
-        try:
-        	result = rds_cursor.fetchall()
-        except _mysql_exceptions.Error, err:
-        	logger.error(err, exc_info=True)
-        	sys.exit(1)
-        print result
+            print(err)
+            sys.exit(1)
+
+    def upload_csv_files_to_s3(self):
+        s3 = boto3.client('s3',
+                          aws_access_key_id=conf.get('aws','aws_access_key_id'),
+                          aws_secret_access_key=conf.get('aws','aws_secret_access_key'),
+                          region_name=conf.get('aws', 'region_name')
+                          )
+        for table in tables:
+            tempfile = "/Users/sandeep/Documents/project/code/experiments/tython/csv_files/"+ table + ".csv"
+            fil = open(tempfile, "rb")
+            s3_path = "tipocaData/bckp_" + suffix + "/" + table
+            response = s3.put_object(
+                Bucket=conf.get('aws', 'bucket'),
+                Key=s3_path,
+                ACL="private",
+                Body=fil
+            )
