6ab383cb7f92087c45f954f10640b6cc8e8053e2
SandeepPandey <s4n989@gmail.com>
2016-11-29 02:39:32 +0530

    Added support for uploading data to redshift and auto reading the columns in schema

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 # Class containgin all the required methods for data differential extractions
 ##
 
-import rds_handler
+import rds_handler, redshift_handler
 import time
 from datetime import datetime, date
 import dateutil.parser
@@ -10,6 +10,7 @@ import dateutil.parser
 def create_rds_snapshot_instance():
     print("Creating rds instance using snapshot")
     rds_handler_object = rds_handler.RdsHandler()
+    redshift_handler_object = redshift_handler.RedshiftHandler()
     latest_snapshot = rds_handler_object.get_snapshot_list()
     print latest_snapshot
     print latest_snapshot["SnapshotCreateTime"]
@@ -19,4 +20,6 @@ def create_rds_snapshot_instance():
     #rds_handler_object.create_rds_instance_from_snapshot(latest_snapshot)
     #rds_handler_object.create_redshift_schema()
     #rds_handler_object.generate_data_csv_file()
-    rds_handler_object.upload_csv_files_to_s3()
+    #rds_handler_object.upload_csv_files_to_s3()
+    #redshift_handler_object.upload_csv_redshift()
+
--- app/rds_handler.py
+++ app/rds_handler.py
@@ -8,6 +8,7 @@ import MySQLdb
 import MySQLdb.cursors
 import _mysql_exceptions
 import sys
+from datetime import datetime
 import math
 import csv
 
@@ -25,7 +26,17 @@ tables = ["applicant_profile", "applicant_profile_version", "applicant_status", 
                   "interviewer_interview_relations", "interviewer_status", "load_data", "project_requisition_relations",
                   "projects", "requisition", "roles", "rounds", "sources", "test", "transaction", "universities",
                   "user_requisition_relations", "users"]
-
+# columns = {
+#     "applicant_profile" : ["id","applicant_id","recruiter_box_id","name","contact","email","experience","location","degree_id","university_id","current_company_id","previous_company_1_id","previous_company_2_id"],
+#     "applicant_profile_version": ["id","applicant_id","recruiter_box_id","name","contact","email","experience","location","degree_id","university_id","current_company_id","previous_company_1_id","previous_company_2_id","applicants_profile_version_id","transaction_id","operation_type","end_transaction_id"],
+#     "applicant_status":["id","name"],
+#     "applicants":["id","recruiter_box_id","project_id","source_id","cv_shared_date","cv_status_id","applicant_status_id","cv_screening_date","recruiter_id","requisition_id","name","email","referrer_email","applicant_status_populated_from"],
+#     "applicant_version":["id","recruiter_box_id","project_id","requisition_id","source_id","cv_shared_date","cv_status_id","applicant_status_id","cv_screening_date","recruiter_id","transaction_id","operation_type","end_transaction_id","applicants_version_id","name","email","referrer_email","applicant_status_populated_from"],
+#     "candidate_metadata":["id","applicant_id","key","value"],
+#     "companies":["id","name"],
+#
+#
+# }
 class RdsHandler:
     def get_snapshot_list(object):
         snapshots = conn.describe_db_snapshots(
@@ -57,13 +68,49 @@ class RdsHandler:
                 port=int(conf.get('hireninja', 'port')),
                 db=conf.get('hireninja', 'db'),
                 cursorclass=MySQLdb.cursors.DictCursor)
-            rds_cursor = rds.cursor()
             for table in tables:
-                rds_cursor.execute("select * from " + table);
+                rds_cursor = rds.cursor()
+                rds_cursor.execute("select group_concat(COLUMN_NAME) as columns from information_schema.COLUMNS where TABLE_SCHEMA = \"hireninja\" and TABLE_NAME = \"" + table + "\"");
+                column = rds_cursor.fetchall()
+                column = column[0]["columns"]
+                column = column.split(',')
+                column_val = []
+                for col in column:
+                    column_val.append("`"+col+"`")
+                print "select " + ",".join(column_val) + " from " + table
+                rds_cursor.execute("select "+",".join(column_val)+" from " + table);
                 result = rds_cursor.fetchall()
-                myfile = open("/Users/sandeep/Documents/project/code/experiments/tython/csv_files/" + table + ".csv", 'wb')
-                wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-                wr.writerow(result)
+                fp = open("/Users/sandeep/Documents/project/code/experiments/tython/csv_files/" + table + ".csv", "wb")
+                for row in result:
+                    col_data = []
+                    for col in column:
+                        col = row[col]
+                        if col is None:
+                            col_data.append(u"")
+                        elif isinstance(col, datetime):
+                            temp = unicode(col.isoformat(" ").split(".")[0])
+                            col_data.append(u'"' + temp + u'"')
+                        elif isinstance(col, str):
+                            temp = col
+                            temp = temp.replace("\0", "")
+                            if len(temp) > 20000:
+                                temp = temp[0:20000]
+                            temp = temp.replace('"', '""')
+                            temp = temp.decode('unicode_escape')
+                            col_data.append(u'"' + unicode(temp) + u'"')
+                        elif isinstance(col, unicode):
+                            temp = col
+                            temp = temp.replace("\0", "")
+                            if len(temp) > 20000:
+                                temp = temp[0:20000]
+                            temp = temp.replace('"', '""')
+                            col_data.append(u'"' + temp + u'"')
+                        else:
+                            col_data.append(unicode(str(col)))
+                    row_csv = u",".join(col_data) + u"\n"
+                    fp.write(row_csv.encode('utf-8'))
+                fp.close()
+                rds_cursor.close()
         except _mysql_exceptions.Error, err:
             print(err)
             sys.exit(1)
